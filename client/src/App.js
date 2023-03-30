9a6e0efc01a3c7588d5ff69f96c644350df47208
David Harvey <58657636+ddharvey@users.noreply.github.com>
2023-03-30 17:51:54 +0100

    Update App.js with generated code

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,80 @@
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-function App() {
-  const [items, setItems] = useState([]);
+const currencies = [
+  { name: "US Dollars", abbreviation: "USD" },
+  { name: "Euro", abbreviation: "EUR" },
+  { name: "Japanese Yen", abbreviation: "JPY" },
+  { name: "British Pound Sterling", abbreviation: "GBP" },
+  { name: "Australian Dollar", abbreviation: "AUD" },
+  { name: "Canadian Dollar", abbreviation: "CAD" },
+  { name: "Swiss Franc", abbreviation: "CHF" },
+  { name: "Chinese Yuan Renminbi", abbreviation: "CNY" },
+  { name: "Hong Kong Dollar", abbreviation: "HKD" },
+  { name: "New Zealand Dollar", abbreviation: "NZD" }
+];
 
-  const addItem = (event) => {
-    event.preventDefault();
-    const newItem = event.target.elements.item.value.trim();
-    if (newItem !== "") {
-        setItems([...items, newItem]);
-        event.target.elements.item.value = "";
-    }
-  }
+const App = () => {
+  const [fromCurrency, setFromCurrency] = useState(currencies[0].abbreviation);
+  const [toCurrency, setToCurrency] = useState(currencies[1].abbreviation);
+  const [amount, setAmount] = useState(1);
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const convertCurrency = () => {
+    const fromCurrencyRate = 1; // Replace this with actual API call to get real-time currency conversion rates
+    const toCurrencyRate = 1; // Replace this with actual API call to get real-time currency conversion rates
+    const calculatedAmount = (amount / fromCurrencyRate) * toCurrencyRate;
+    setConvertedAmount(calculatedAmount);
+  };
 
   return (
     <div>
-      <h1>Shopping List</h1>
-      <form onSubmit={addItem}>
-        <input type="text" name="item" placeholder="Add item..." />
-        <button>Add</button>
-      </form>
-      <ul>
-        {items.map((item, index) => {
-          return <li key={index}>{item}</li>
-        })}
-      </ul>
+      <h1>Currency Converter</h1>
+      <div>
+        <label htmlFor="from-currency">From Currency:</label>
+        <select
+          id="from-currency"
+          value={fromCurrency}
+          onChange={(e) => setFromCurrency(e.target.value)}
+        >
+          {currencies.map((currency) => (
+            <option key={currency.abbreviation} value={currency.abbreviation}>
+              {currency.name} ({currency.abbreviation})
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="to-currency">To Currency:</label>
+        <select
+          id="to-currency"
+          value={toCurrency}
+          onChange={(e) => setToCurrency(e.target.value)}
+        >
+          {currencies.map((currency) => (
+            <option key={currency.abbreviation} value={currency.abbreviation}>
+              {currency.name} ({currency.abbreviation})
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="amount">Amount:</label>
+        <input
+          id="amount"
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={convertCurrency}>Convert</button>
+      </div>
+      <div>
+        {amount} {fromCurrency} = {convertedAmount} {toCurrency}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
