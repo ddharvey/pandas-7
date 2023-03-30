530a9d73079c2353cdc76a7efbe85e8c1b14f8dd
David Harvey <58657636+ddharvey@users.noreply.github.com>
2023-03-30 17:49:10 +0100

    Update App.js with generated code

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const addItem = (event) => {
+    event.preventDefault();
+    const newItem = event.target.elements.item.value.trim();
+    if (newItem !== "") {
+        setItems([...items, newItem]);
+        event.target.elements.item.value = "";
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Shopping List</h1>
+      <form onSubmit={addItem}>
+        <input type="text" name="item" placeholder="Add item..." />
+        <button>Add</button>
+      </form>
+      <ul>
+        {items.map((item, index) => {
+          return <li key={index}>{item}</li>
+        })}
+      </ul>
     </div>
   );
 }
